fc7b694056e0d1a44ac04ae1621bab8130a12dee
Aditya Verma <adityarubbers.av@gmail.com>
2022-01-11 11:57:02 +0530

    Made changes

--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
-// function populateGallery(images) {
-//     let carouselItems = images.map((image, index) => {
-//         return `
-//         <div class="carousel-item ${(index == 0)? "active": ""}">
-//             <img src="${image}" class="d-block w-100" alt="Hemant and pracheta">
-//         </div>
-//     `
-//     }).reduce((prev, current) => prev.concat(current), "");
-//     let gallery = document.getElementById("gallery-carousel").getElementsByClassName("carousel-inner")[0];
-//     gallery.innerHTML = carouselItems;
-// }
-
-// populateGallery(["assets/landing.jpeg", "assets/landing.jpeg","assets/landing.jpeg","assets/landing.jpeg","assets/landing.jpeg","assets/landing.jpeg"]);
-
-var preloaderFadeOutTime = 5500
-var preloader = $("#loader");
-preloader.fadeOut(preloaderFadeOutTime);
+$(window).on("load", function () {
+  var preloaderFadeOutTime = 1000;
+  var preloader = $("#loader");
+  preloader.fadeOut(preloaderFadeOutTime);
+});
 new fullpage("#fullpage", {
-    controlArrows: true,
-    verticalCentered: false,
-    navigation: true,
-    navigationTooltips: ['Home', 'Bride & Groom', 'Events', 'Gallery', 'Video'],
-    scrollOverflow: true
+  controlArrows: true,
+  verticalCentered: false,
+  navigation: true,
+  navigationTooltips: ["Home", "Bride & Groom", "Events", "Gallery", "Video"],
+  scrollOverflow: true,
 });
