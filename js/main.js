a4e35616b2cfe579d1894c175121d359f098af0c
Aditya Verma <adityarubbers.av@gmail.com>
2022-01-12 12:07:34 +0530

    Added lightgallery

--- js/main.js
+++ js/main.js
@@ -10,3 +10,31 @@ new fullpage("#fullpage", {
   navigationTooltips: ["Home", "Bride & Groom", "Events", "Gallery", "Video"],
   scrollOverflow: true,
 });
+lightGallery(document.getElementById("lightgallery"), {
+  plugins: [lgZoom, lgThumbnail],
+  speed: 500,
+});
+$("#animated-thumbnails-gallery")
+  .justifiedGallery({
+    captions: false,
+    lastRow: "hide",
+    rowHeight: 180,
+    margins: 5,
+  })
+  .on("jg.complete", function () {
+    window.lightGallery(
+      document.getElementById("animated-thumbnails-gallery"),
+      {
+        autoplayFirstVideo: false,
+        pager: false,
+        galleryId: "nature",
+        plugins: [lgZoom, lgThumbnail],
+        mobileSettings: {
+          controls: false,
+          showCloseIcon: false,
+          download: false,
+          rotate: false,
+        },
+      }
+    );
+  });
