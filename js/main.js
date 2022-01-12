1e2f7e1a12bcd389964248384114e97e7975eafb
Hemant Bhoyar <hybhoyar@gmail.com>
2022-01-12 13:09:46 +0530

    Loading gallerys after window load

--- js/main.js
+++ js/main.js
@@ -2,7 +2,32 @@ $(window).on("load", function () {
   var preloaderFadeOutTime = 1000;
   var preloader = $("#loader");
   preloader.fadeOut(preloaderFadeOutTime);
+
+  $("#animated-thumbnails-gallery")
+    .justifiedGallery({
+      captions: false,
+      lastRow: "hide",
+      rowHeight: 180,
+      margins: 5
+    }).on("jg.complete", function () {
+      window.lightGallery(
+        document.getElementById("animated-thumbnails-gallery"),
+        {
+          autoplayFirstVideo: false,
+          pager: false,
+          galleryId: "nature",
+          plugins: [lgZoom, lgThumbnail],
+          mobileSettings: {
+            controls: false,
+            showCloseIcon: false,
+            download: false,
+            rotate: false,
+          },
+        }
+      );
+    });
 });
+
 new fullpage("#fullpage", {
   controlArrows: true,
   verticalCentered: false,
@@ -11,27 +36,3 @@ new fullpage("#fullpage", {
   scrollOverflow: true,
 });
 
-
-$("#animated-thumbnails-gallery")
-  .justifiedGallery({
-    captions: false,
-    lastRow: "hide",
-    rowHeight: 180,
-    margins: 5
-  }).on("jg.complete", function () {
-    window.lightGallery(
-      document.getElementById("animated-thumbnails-gallery"),
-      {
-        autoplayFirstVideo: false,
-        pager: false,
-        galleryId: "nature",
-        plugins: [lgZoom, lgThumbnail],
-        mobileSettings: {
-          controls: false,
-          showCloseIcon: false,
-          download: false,
-          rotate: false,
-        },
-      }
-    );
-  });
